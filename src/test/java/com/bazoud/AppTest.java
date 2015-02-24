c41d03d6012d5c1b5dc39067a8f397f0c3d2b7ed
Olivier Bazoud <olivier.bazoud@gmail.com>
2015-02-24 17:07:17 +0100

    Project generation

---
+++ src/test/java/com/bazoud/AppTest.java
@@ -0,0 +1,14 @@
+package com.bazoud;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test_for_dummy() {
+        assertThat(true, is(true));
+    }
+}
